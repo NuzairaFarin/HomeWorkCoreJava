1e1c168c2ce4df34b19ddd5dc57fdd7af415ddcd
Nuzaira Farin <nuzairafarin@nuzairas-mbp.myfiosgateway.com>
2022-11-02 21:22:54 -0400

    9.22PM

--- src/hw5Q3Constructor/StudentTest.java
+++ src/hw5Q3Constructor/StudentTest.java
@@ -5,6 +5,7 @@ public class StudentTest {
 	public static void main(String[] args) {
 		Student student = new Student();
 		Student student1 = new Student("Nuzaira", 982633, 'F', true, 3.85f);
+		Student student2 = new Student("Nuzaira", 9833, 'F', true, 3.85f);
 
 	}
 
